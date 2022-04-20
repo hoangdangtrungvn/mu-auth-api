288aa9fcbf7bb212a2440d052036b871bb1ef3fc
XinSo.Dev <xinso.dev@gmail.com>
2022-04-20 19:14:43 +0700

    fix: strip validated object of any properties that do not use any validation decorators

--- src/main.ts
+++ src/main.ts
@@ -17,6 +17,7 @@ async function bootstrap() {
       transform: true,
       stopAtFirstError: false,
       exceptionFactory: ExceptionFactory,
+      whitelist: true,
     }),
   )
   app.useGlobalFilters(new HttpExceptionFilter())
